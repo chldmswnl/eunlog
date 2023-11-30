fddd885fdc5af424180a47cbf3f105a0f2e5aa47
chldmswnl <chldmswnl12@gmail.com>
2023-11-30 20:48:46 +0900

    feat: work with posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import PostSection from "@/components/PostSection";
+import { useState } from "react";
+
 export default function Posts() {
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const categoryArr = [
+    "All posts",
+    "my story",
+    "frontend",
+    "backend",
+    "javascript",
+  ];
+
   return (
-    <div className="w-4/5 bg-gray-200 p-20 flex-col">
-      <span>hello</span>
-    </div>
+    <section className="flex mb-5">
+      {/* <PostSection category={selectedCategory} /> */}
+      <article className="mx-4">
+        <div className="flex flex-col items-center">
+          <h3 className="font-bold border-b-2 border-slate-800 mb-3">
+            Category
+          </h3>
+          {categoryArr.map((category) => (
+            <span
+              key={category}
+              className="font-semibold text-neutral-800 cursor-pointer"
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </span>
+          ))}
+        </div>
+      </article>
+    </section>
   );
 }
