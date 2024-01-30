24efe52310d8156e54474a780a0f5960aae85abb
chldmswnl <chldmswnl12@gmail.com>
2024-01-30 23:18:33 +0900

    chore: delete about and contact page

--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="w-4/5 bg-gray-200 p-20 flex-col">
-      <span>about page</span>
-    </div>
-  );
-}
--- src/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="w-4/5 bg-gray-200 p-20 flex-col">
-      <span>contact page</span>
-    </div>
-  );
-}
