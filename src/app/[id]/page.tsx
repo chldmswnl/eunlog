a72222f62a1e282365e5235d9cc23afcab0a660c
chldmswnl <chldmswnl12@gmail.com>
2024-06-27 16:07:21 +0900

    feat: routing 추가

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,7 @@
-export default function PostList() {
+export default function Post({ params: { id } }: { params: { id: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span>Post list page</span>
+      <span>Post page {id}</span>
     </main>
   )
 }
