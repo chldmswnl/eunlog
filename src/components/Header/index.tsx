65047dc88b35bd775819f09f7ab0d9a99a3c74b0
chldmswnl <chldmswnl12@gmail.com>
2024-06-27 17:37:43 +0900

    style: Header 스타일 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="w-full max-w-screen-md bg-white">
-      <span>Header</span>
+    <header className="h-40 w-full max-w-screen-md py-6">
+      <span className="text-3xl font-bold text-slate-800">Eunlog</span>
     </header>
   )
 }
