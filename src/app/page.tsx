1fdc2fef4167867a114c06a7d7e3c72974929630
chldmswnl <chldmswnl12@gmail.com>
2024-07-03 20:53:36 +0900

    feat: CardGrid component 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CardGrid from '@/components/CardGrid'
 import ProfileSection from '@/components/ProfileSection'
 
 export default function PostList() {
   return (
-    <section>
+    <section className="flex flex-col gap-y-10">
       <ProfileSection />
+      <CardGrid />
     </section>
   )
 }
