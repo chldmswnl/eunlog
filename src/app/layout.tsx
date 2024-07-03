fe1ef04bb8d61a25953230459ae692d0dc012e14
chldmswnl <chldmswnl12@gmail.com>
2024-07-03 20:52:50 +0900

    style: 스타일 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.className}>
-      <body className="flex h-screen w-full flex-col items-center bg-slate-300">
+      <body className="flex w-full flex-col items-center">
         <Header />
-        <main className="h-full w-full max-w-screen-md bg-white">
-          {children}
-        </main>
+        <main className="w-full max-w-screen-md grow">{children}</main>
         <Footer />
       </body>
     </html>
