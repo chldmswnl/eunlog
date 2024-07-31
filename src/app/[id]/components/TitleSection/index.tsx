48f06e47d39f1d34d42e91a049313e07bd54864c
chldmswnl <chldmswnl12@gmail.com>
2024-07-31 18:12:16 +0900

    chore: Title section 추가

---
+++ src/app/[id]/components/TitleSection/index.tsx
@@ -0,0 +1,26 @@
+import { PostMetadataType } from '@/type'
+import { getFormattedDate } from '@/utils/date'
+
+export default function TitleSection({
+  metaData,
+}: {
+  metaData: PostMetadataType
+}) {
+  const { title, keyword, date } = metaData
+
+  return (
+    <section
+      style={{
+        borderTop: '1px solid rgb(212 212 212)',
+        borderBottom: '1px solid rgb(212 212 212)',
+      }}
+      className="mb-10 flex h-52 w-full flex-col items-center justify-center gap-y-4"
+    >
+      <h1 className="block text-3xl font-bold">{title}</h1>
+      <h2 className="block text-lg font-bold text-[#E38AA2]">{keyword}</h2>
+      <span className="block text-sm font-semibold text-gray-600">
+        {getFormattedDate(new Date(date))}
+      </span>
+    </section>
+  )
+}
