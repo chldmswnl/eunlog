532767a470ce75e5048ef3d69bc06ce73242967e
chldmswnl <chldmswnl12@gmail.com>
2024-08-07 19:26:24 +0900

    chore: 임시 RecommendSection 추가

---
+++ src/app/[id]/components/RecommendPostSection/index.tsx
@@ -0,0 +1,11 @@
+export default function RecommendPostSection() {
+  return (
+    <section className="my-10 w-full gap-y-5">
+      <span className="mb-5 block text-xl font-bold">Yoy may also like</span>
+      {/* <div className="flex justify-py-5">
+        <span className="text-base font-bold">여름 이야기</span>
+        <span className="text-base font-bold">frontend</span>
+      </div> */}
+    </section>
+  )
+}
