486865bcdc46efd912fbf49b9330d35bcbcb1931
chldmswnl <chldmswnl12@gmail.com>
2024-08-07 21:48:59 +0900

    feat: 동적 메타데이터 추가

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,16 +1,35 @@
-import { parsePost } from '@/utils/parsePost'
+import { parsePost } from '@/utils/post'
 
 import TitleSection from './components/TitleSection'
-import RecommendPostSection from './components/RecommendPostSection'
 import Giscus from '@/components/Giscus'
 import MdxSection from './components/MdxSection'
+import { Metadata } from 'next'
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = params.id
+
+  const { metadata } = await parsePost(`src/data/${id}.mdx`)
+
+  return {
+    title: metadata.title,
+    openGraph: {
+      title: metadata.title,
+      description: metadata.desc,
+    },
+  }
+}
 
 export default async function Post({
   params: { id },
 }: {
   params: { id: string }
 }) {
-  const { metadata, content } = await parsePost('src/data/post.mdx')
+  const { metadata, content } = await parsePost(`src/data/${id}.mdx`)
 
   return (
     <section>
