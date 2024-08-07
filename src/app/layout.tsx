f6d421a79845c526c635c5eb4c3adb55c8db0ab8
chldmswnl <chldmswnl12@gmail.com>
2024-08-07 19:25:28 +0900

    feat: og 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Eunlog',
   description: '꾸준함의 힘을 믿는 프론트엔드 개발자의 기술블로그',
+  openGraph: {
+    title: 'Eunlog',
+    description: '꾸준함의 힘을 믿는 프론트엔드 개발자의 기술블로그',
+    images: {
+      url: 'https://github.com/user-attachments/assets/a41f35c2-02c5-48af-9093-b8e8a953be43', // Must be an absolute URL
+      width: 1200,
+      height: 630,
+    },
+  },
 }
 
 export default function RootLayout({
